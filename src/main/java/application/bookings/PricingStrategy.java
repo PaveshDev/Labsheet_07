571bd1548bc08b17b1a7065b6f7d2282126cd7ba
PaveshDev <pavesh2003@gmail.com>
2025-10-13 18:17:27 +0530

    Revert "Add subsystem patterns for hotel management modules"

--- src/main/java/application/bookings/PricingStrategy.java
+++
@@ -1,8 +0,0 @@
-package application.bookings;
-
-/**
- * Strategy for calculating the final price of a booking.
- */
-public interface PricingStrategy {
-    double calculatePrice(double baseNightlyRate, int nights);
-}
--- src/main/java/application/promotions/PromotionSubscriber.java
+++
@@ -1,8 +0,0 @@
-package application.promotions;
-
-/**
- * Observer that reacts to promotion updates.
- */
-public interface PromotionSubscriber {
-    void onPromotion(PromotionEvent event);
-}
